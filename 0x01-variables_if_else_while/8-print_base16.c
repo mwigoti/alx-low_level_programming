950c4600a695fdeb14e68a14a1f93bd95ee2aaf4
mwigoti <henrymwoha02@gmail.com>
2023-02-20 09:23:30 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: '16 base'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 48;
+	int m = 97;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m < 103; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}	
