46b0df29a0639b1da617a4b2c5bb6165d2e16f24
mwigoti <henrymwoha02@gmail.com>
2023-02-23 10:03:22 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}
