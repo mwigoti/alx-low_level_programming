56cc9280ea64c60c93f87e1cb991d78e8a6921b6
mwigoti <henrymwoha02@gmail.com>
2023-03-02 07:21:08 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * Return: difference of unmatched first characters
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
