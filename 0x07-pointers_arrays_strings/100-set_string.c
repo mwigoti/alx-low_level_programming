f4d6db5c3e2e1fe76d56bcc0ce73d9fef86b11ae
mwigoti <henrymwoha02@gmail.com>
2023-03-06 13:04:55 -0800

    df

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of pointrto char
+ * @s: pointer o pointer
+ * @to: pointer character
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
